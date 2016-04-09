48316924b4e1c0235774a7ad757786443e1833a0
Jerry Zhao <xyzjerry@gmail.com>
2016-04-08 19:53:16 -0700

    add publish date default in datamodel and clean some unused code.

--- video.js
+++ video.js
@@ -5,7 +5,7 @@ var VideoSchema = new mongoose.Schema({
   videoid: String,
   title: String,
   description: String,
-  publish_date: Date,
+  publish_date: {type: Date, default: Date.now},
   googleID: Number
 });
 
