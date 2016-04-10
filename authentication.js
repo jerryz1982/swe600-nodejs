c58e05c299819260a72d0803fce2d02d0bc44d4f
Jerry Zhao <xyzjerry@gmail.com>
2016-04-09 19:52:32 -0700

    remove youtube-api dependency

--- authentication.js
+++ authentication.js
@@ -2,7 +2,6 @@ var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
 var User = require('./user.js');
 var Video = require('./video.js');
-var youtube = require('youtube-api');
 var ReadJson = require("r-json");
 const CREDENTIALS = ReadJson("./client_secret_twitter.json");
 
